66984dc8148ee8ec38e726632ae4e7e98928ea62
Christophe Hurpeau <christophe@hurpeau.com>
2012-07-15 19:08:44 +0200

    VPostsLatest

--- blog/viewsElements/PostsLatest/VPostsLatest.php
+++ blog/viewsElements/PostsLatest/VPostsLatest.php
@@ -3,7 +3,7 @@ class VPostsLatest extends SViewCachedElement{
 	/* DEV */ public function exists(){ return false; } /* /DEV */
 	public static function path(){return DATA.'elementsCache/posts/latest-list';}
 	public static function vars(){
-		$posts=Post::QListAll()->addField('excerpt')->limit(4);
+		$posts=Post::QListAll()->addField('excerpt')->limit(/* VALUE(blog.VPostsLatest.size) */);
 		foreach($posts as $post)
 			$post->excerpt=UHtml::transformInternalLinks($post->excerpt,Config::$internalLinks);
 		return array(
