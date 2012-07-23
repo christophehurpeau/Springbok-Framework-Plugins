0e81799232c98d1c61bb025cf4525c37fa855e58
Christophe Hurpeau (A2Zi) <christophe.hurpeau@a2zi.fr>
2012-07-23 10:20:55 +0200

    url

--- blog/viewsLayouts/admin/cms.php
+++ blog/viewsLayouts/admin/cms.php
@@ -3,14 +3,14 @@
 
 <div class="fixed left w160">
 	{menuLeft 'startsWith':true/*,'menuAttributes':array('rel'=>'page')*/
-		_t('plugin.blog.Posts')=>array('url'=>'/posts','startsWith'=>false),
+		_t('plugin.blog.Posts')=>array('/posts','startsWith'=>false),
 		/* IF(blog_personalizeAuthors_enabled) */
 		_t('plugin.blog.Authors')=>'/postsAuthors',
 		/* /IF */
 		_t('plugin.blog.Tags')=>'/postsTags',
 		_t('plugin.blog.Categories')=>'/postsCategories',
 		/* IF(blog_comments_enabled) */
-		_t('plugin.blog.Comments')=>array('url'=>'/postsComments','startsWith'=>false),
+		_t('plugin.blog.Comments')=>array('/postsComments','startsWith'=>false),
 		_t('plugin.blog.CommentValidation')=>'/postsComments/validation',
 		/* /IF */
 		false,
@@ -18,7 +18,7 @@
 	}
 	<hr/>
 	{menuLeft 'startsWith':true/*,'menuAttributes':array('rel'=>'page')*/
-		_t('plugin.cms.Pages')=>array('url'=>'/pages','startsWith'=>false),
+		_t('plugin.cms.Pages')=>array('/pages','startsWith'=>false),
 		false,
 		_t('plugin.cms.Tools')=>'/pages/tools',
 	}
