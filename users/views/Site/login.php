d1053e70aa08b882cd6457ba6a66615a4e70926f
Christophe Hurpeau (A2Zi) <christophe.hurpeau@a2zi.fr>
2012-09-03 09:32:20 +0200

    users

--- users/views/Site/login.php
+++ users/views/Site/login.php
@@ -1,7 +1,6 @@
-<?php new PageView(_tC('Sign in')) ?>
+<?php new AjaxPageView(_tC('Sign in')) ?>
 
 <? CSession::flash('user/login') ?>
-<div id="page">
 <div class="clear mt20">
 <?php $form=HForm::create('User',array('class'=>'w400 centered big','action'=>'/site/login'));
 echo '<h2>'._tC('Sign in').'</h2>';
@@ -13,5 +12,4 @@ echo $form->submit(_tC('Sign in'),array(),array('class'=>'center'));
 /*echo '<br class="clear">';
 echo HHtml::tag('div',array('class'=>'center','style'=>'float:none'),HHtml::link(_tC('Password lost ?'),'/site/lostPassword'),false);*/
 $form->end(false); ?>
-</div>
 </div>
