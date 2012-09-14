c1ab2d95568abe57b003941e001346736596cbfa
Christophe Hurpeau (A2Zi) <christophe.hurpeau@a2zi.fr>
2012-09-14 09:27:56 +0200

    bug

--- base/viewsLayouts/Dev/base.php
+++ base/viewsLayouts/Dev/base.php
@@ -4,7 +4,7 @@
 		<? HHtml::metaCharset() ?>
 		<title><?= Config::$projectName ?> - {$layout_title}</title>
 		<!--[if lt IE 9]><?php HHtml::jsLink('/ie-lt9') ?><![endif]-->
-		<?php HHtml::cssLink('/Dev/main'); HHtml::jsLink('/Dev/global'); HHtml::jsI18n() ?>
+		<?php HHtml::cssLink('/Dev/dev'); HHtml::jsLink('/Dev/dev'); HHtml::jsI18n() ?>
 		<? HHtml::favicon() ?>
 	</head>
 	<body>{=$layout_content}</body>
