fca6a35b8686493047616c7b6ae040dd0b31e4a0
Christophe Hurpeau (A2Zi) <christophe.hurpeau@a2zi.fr>
2012-10-22 17:58:06 +0200

    Blog : VPostsLatest titles

---
+++ blog/viewsElements/PostsLatest/titles.php
@@ -0,0 +1,17 @@
+<ul class="nobullets cSepTop">
+	{f $posts as $post}
+	<li>
+		<?php $postLink=$post->link() ?>
+		<article itemscope itemtype="http://schema.org/Article" class="clearfix">
+			{if!null $post->image->image_id}
+				<?php $url=STATIC_URL.'/files/cms_images/'.$post->image->image_id; ?>
+				{link '<img class="floatL mr10" itemprop="image" content="'.$url.'.jpg" width="75" height="75" src="'.$url.'-medium.jpg" />',$postLink,array('escape'=>false)}
+			{/if}
+			<h3 class="noclear" itemprop="name">{link $post->name,$postLink,array('itemprop'=>'url')}</h3>
+			<span itemprop="dateCreated" content="{$post->created}"></span>
+			<span itemprop="datePublished" content="{$post->published}"></span>
+			{if!null $post->updated}<span itemprop="dateModified" content="{$post->updated}"></span>{/if}
+		</article>
+	</li>
+	{/f}
+</ul>
