018d904ccefcb1907b285f2a0e6a5bf140193301
Christophe Hurpeau (A2Zi) <christophe.hurpeau@a2zi.fr>
2012-12-04 10:14:26 +0100

    js compat

--- base/viewsLayouts/Dev/base.php
+++ base/viewsLayouts/Dev/base.php
@@ -3,7 +3,7 @@
 	<head>
 		<? HHtml::metaCharset() ?>
 		<title><?= Config::$projectName ?> - {$layout_title}</title>
-		<!--[if lt IE 9]><?php HHtml::jsLink('/ie-lt9') ?><![endif]-->
+		<? HHtml::jsCompat() ?>
 		<?php HHtml::cssLink('/Dev/dev'); HHtml::jsLink('/Dev/dev'); HHtml::jsI18n() ?>
 		<? HHtml::favicon() ?>
 	</head>
