1ebf099c185e17c8c403564a068d047394ac3261
Christophe Hurpeau <christophe@hurpeau.com>
2013-01-25 23:28:45 +0100

    [blog] dbEvolution 2013-01-24 15

--- blog/dbEvolutions/2013-01-24 15:00:00.sql
+++ blog/dbEvolutions/2013-01-24 15:00:00.sql
@@ -1,4 +1,6 @@
 default=>ALTER TABLE post_tags DROP FOREIGN KEY fk_post_tags_tag_id,DROP FOREIGN KEY fk_post_tags_post_id, DROP PRIMARY KEY
 default=>UPDATE post_tags pt LEFT JOIN posts_tags pst ON pst.id=pt.tag_id SET pt.tag_id=pst.p_id
-default=>ALTER TABLE posts_tags CHANGE `id` `id` INT( 10 ) UNSIGNED NOT NULL ,DROP PRIMARY KEY
+default=>ALTER TABLE posts_tags CHANGE `id` `id` INT( 10 ) UNSIGNED NOT NULL, DROP FOREIGN KEY fk_posts_tags_p_id
+default=>ALTER TABLE posts_tags DROP INDEX unique_p_id
+default=>ALTER TABLE posts_tags DROP PRIMARY KEY
 default=>UPDATE posts_tags SET id=p_id
