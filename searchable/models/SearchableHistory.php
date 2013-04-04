c8e2d6fbda0f1cfbfb72f0e9056e5c43a17a8673
Christophe Hurpeau <christophe@hurpeau.com>
2013-04-04 17:02:06 +0200

    [searchable] history

---
+++ searchable/models/SearchableHistory.php
@@ -0,0 +1,40 @@
+<?php
+/** @TableName('searchable_history') @TableAlias('sh') @Created */
+class SearchableHistory extends SSqlModel{
+	/* [01][0-9] RESERVED */
+	const 
+		CREATED=01,UPDATED=02,DELETED=03,STATUS_CHANGED=04,
+		MERGED=05,MERGED_TO=06
+	;
+	
+	
+	public
+		/** @Pk @AutoIncrement @SqlType('BIGINT(20) unsigned') @NotNull
+		*/ $id,
+		/** @SqlType('int(10) unsigned') @NotNull
+		* @ForeignKey('Searchable','id')
+		*/ $searchable_id,
+		/** @SqlType('int(10) unsigned') @Null
+		* @ForeignKey('User','id')
+		*/ $user_id,
+		/* IF(searchableHistory.source) */
+		/** @SqlType('tinyint(2) unsigned') @NotNull
+		* @Enum(['AConsts','sources'])
+		*/ $source,
+		/* /IF */
+		/** @SqlType('tinyint(2) unsigned') @NotNull @Index
+		*/ $type,
+		/** @SqlType('int(10) unsigned') @Null
+		*/ $rel_id;
+	
+	public static function add($searchableId,$type,$relId=null,$userId=true,$source=AConsts::DEFAULT_SOURCE){
+		if($userId===true) $userId=CSecure::connected();
+		$oh=new self;
+		$oh->searchable_id=$searchableId;
+		$oh->user_id=$userId;
+		$oh->type=$type;
+		/* IF(searchableHistory.source) */ $oh->source=$source; /* /IF */
+		if($relId !== null) $oh->rel_id=$relId;
+		$oh->insert();
+	}
+}
