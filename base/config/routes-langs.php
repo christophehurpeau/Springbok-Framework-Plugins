7e6e0d833e948793f2639a9467f9062ce59237cd
Christophe Hurpeau <christophe@hurpeau.com>
2013-04-10 12:56:28 +0200

    [base] add default routes langs

---
+++ base/config/routes-langs.php
@@ -0,0 +1,10 @@
+<?php return array(
+	'robots'=>array('fr'=>'robots','es'=>'robots','it'=>'robots'),
+	'favicon'=>array('fr'=>'favicon','es'=>'favicon','it'=>'favicon'),
+	
+	'view'=>array('fr'=>'afficher'),
+	'edit'=>array('fr'=>'editer'),
+	'update'=>array('fr'=>'modifier'),
+	'add'=>array('fr'=>'ajouter'),
+	'delete'=>array('fr'=>'supprimer'),
+);
