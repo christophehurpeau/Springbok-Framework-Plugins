a5d7fb1bd555ad0cdd781aa990b1143bad791522
Cyril CRETTE <cyril.crette@a2zi.fr>
2013-10-07 14:40:50 +0200

    Revert "Revert "Ajout d'une vue pour lister les actus récentes""
    This reverts commit f91bc34d4622edc6db0f84f8299a8df41023d298.

--- blog/viewsElements/PostsLatest/VPostsLatest.php
+++ blog/viewsElements/PostsLatest/VPostsLatest.php
@@ -1,11 +1,14 @@
 <?php
 class VPostsLatest extends SViewCachedElement{
-	/* DEV */ public function exists(){ return false; } /* /DEV */
-	public static function path(){return DATA.'elementsCache/posts/latest-list';}
+	protected static $views=array('view','titles');
+	
+	/*#if DEV*/ public function exists(){ return false; } /*#/if*/
+	
+	public static function path(){return array('posts','latest-list');}
 	public static function vars(){
-		$posts=Post::QListAll()->addField('excerpt')->limit(/* VALUE(blog.VPostsLatest.size) */);
+		$posts=Post::QListAll()->addField('excerpt')->limit(/*#val blog.VPostsLatest.size */);
 		foreach($posts as $post)
-			$post->excerpt=UHtml::transformInternalLinks($post->excerpt,Config::$internalLinks,'index',/* VALUE(blog.VPostsLatest.fullUrls) *//* HIDE */false/* /HIDE */);
+			$post->excerpt=UHtml::transformInternalLinks($post->excerpt,Config::$internalLinks,'index',/*#val blog.VPostsLatest.fullUrls */false);
 		return array(
 			'posts'=>$posts
 		);
